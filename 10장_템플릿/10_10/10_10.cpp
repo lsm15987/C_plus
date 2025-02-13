de6f9878049fd488a3248395b47ad26f51adc821
Suman Lee <dnjxj0011@naver.com>
2025-02-13 15:02:33 +0900

    10_10 프로젝트 실습
    간단한 vector 컨테이너 실습

---
+++ 10장_템플릿/10_10/10_10.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	vector<int> v;
+
+	v.push_back(1); // 벡터에 정수 1 삽입
+	v.push_back(2);
+	v.push_back(3);
+
+	for (int i = 0; i < v.size(); i++) {
+		cout << v[i] << " ";
+	}
+	cout << endl;
+
+	v[0] = 10;
+	int n = v[2];
+	v.at(2) = 5; // 벡터의 인덱스 2의 원소를 5로 변경
+
+	for (int i = 0; i < v.size(); i++) {
+		cout << v[i] << " ";
+	}
+	cout << endl;
+}
