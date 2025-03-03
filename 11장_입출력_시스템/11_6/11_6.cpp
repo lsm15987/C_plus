34fbfe891d3b32dfbb65a0cdb86af38ba0ac481d
Suman Lee <dnjxj0011@naver.com>
2025-03-03 13:56:08 +0900

    11_6 프로젝트 생성
    포맷 함수 cout.width(), cout.fill(), cout.precision() 실습

---
+++ 11장_입출력_시스템/11_6/11_6.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+void showWidth() {
+	cout.width(10);
+	cout << "Hello" << endl;
+	cout.width(5);
+	cout << 12 << endl;
+
+	cout << '%';
+	cout.width(10);
+	cout << "Korea/" << "Seoul/" << "City" << endl;
+}
+
+int main()
+{
+	showWidth();
+	cout << endl;
+
+	cout.fill('^');
+	showWidth();
+	cout << endl;
+
+	cout.precision(5);
+	double a = 3.123456;
+	cout << a << endl;
+}
