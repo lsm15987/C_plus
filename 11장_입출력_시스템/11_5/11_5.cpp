e03b420edb6c7d2bbff5535a916b7042cb96a194
Suman Lee <dnjxj0011@naver.com>
2025-03-03 13:48:21 +0900

    11_5 프로젝트 생성
    cout.setf()와 cout.setf() 함수 실습

---
+++ 11장_입출력_시스템/11_5/11_5.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << 30 << endl;
+	cout.unsetf(ios::dec);
+	cout.setf(ios::hex);
+	cout << 30 << endl;
+
+	cout.setf(ios::showbase);
+	cout << 30 << endl;
+
+	cout.setf(ios::uppercase);
+	cout << 30 << endl;
+
+	cout.setf(ios::showpoint);
+	cout << 23.5 << endl;
+
+	cout.setf(ios::scientific);
+	cout << 23.5 << endl;
+
+	cout.setf(ios::showpos);
+	cout << 23.5;
+}
